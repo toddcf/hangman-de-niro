3cc47e8364489627799b46c1154ec223309bbf5a
Todd Croak-Falen <toddcf@gmail.com>
2017-05-21 17:45:08 -0700

    Got blanks to display.

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,6 +18,7 @@ $( document ).ready( function() {
 
 	var winCounter = 0;
 	var lossCounter = 0;
+	var blanks = [];
 	var lettersGuessed = [];
 	var guessesLeft = 9;
 
@@ -28,8 +29,26 @@ $( document ).ready( function() {
 
 		// Randomly Select Title:
 		selectedTitle = titles[ Math.floor( Math.random() * titles.length ) ];
+		console.log( selectedTitle );
 		// Split selectedTitle into individual letters:
 		letters = selectedTitle.split( "" );
+		console.log( letters );
+		// Count number of blanks in selected title:
+		var numBlanks = selectedTitle.length;
+		console.log( numBlanks );
+		// Push blanks to blanks array:
+		for ( var i = 0; i < letters.length; i++ ) {
+			// Push spaces:
+			if ( letters[ i ] === " " ) {
+				blanks.push( " ");
+			}
+			else {
+				blanks.push( "_" );
+			}
+		}
+
+		// Print to HTML:
+		document.getElementById( "blanks" ).innerHTML = blanks.join( "  " );
 
 
 	}
