79a63aefbdfb0a07f22483debc83934daa436eef
Todd Croak-Falen <toddcf@gmail.com>
2017-05-21 17:27:17 -0700

    Began function for selecting title.

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,5 +7,49 @@ $( document ).ready( function() {
 	var footerInfo = ( function() {
 		$( ".currentYear" ).html( " &ndash; " + thisYear + " " );
 	}) ();
+
+	var titles = [
+		"GOODFELLAS",
+		"HEAT",
+		"CASINO",
+		"SLEEPERS",
+		"RONIN"
+	];
+
+	var winCounter = 0;
+	var lossCounter = 0;
+	var lettersGuessed = [];
+	var guessesLeft = 9;
+
+	// Function that starts or restarts the game.
+	var play = function() {
+
+		// Hide Start Button
+
+		// Randomly Select Title:
+		selectedTitle = titles[ Math.floor( Math.random() * titles.length ) ];
+		// Split selectedTitle into individual letters:
+		letters = selectedTitle.split( "" );
+
+
+	}
+
+	// Start Game onClick:
+	play();
+
+
+
+
+
+
+
+
+	// One way to add spaces:
+	// if (word[i] === " ") {
+ //                guess.innerHTML = " ";
+ //                space = 1;
+ //            } else {
+ //                guess.innerHTML = "_";
+ //            }
 	
 });
